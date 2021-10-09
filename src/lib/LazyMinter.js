847ff0098439621b2cc8a96a3213038d47047b5c
mohsinanees <mohsin.anees09@gmail.com>
2021-10-09 14:56:41 +0500

    Webaverse

--- src/lib/LazyMinter.js
+++ src/lib/LazyMinter.js
@@ -1,12 +1,12 @@
-const ethers = require('ethers')
+const ethers = require("ethers");
 
 // These constants must match the ones used in the smart contract.
-const SIGNING_DOMAIN_NAME = "LazyNFT-Voucher"
-const SIGNING_DOMAIN_VERSION = "1"
+const SIGNING_DOMAIN_NAME = "Webaverse-voucher";
+const SIGNING_DOMAIN_VERSION = "1";
 
 /**
  * JSDoc typedefs.
- * 
+ *
  * @typedef {object} NFTVoucher
  * @property {ethers.BigNumber | number} tokenId the id of the un-minted NFT
  * @property {ethers.BigNumber | number} minPrice the minimum price (in wei) that the creator will accept to redeem this NFT
@@ -18,43 +18,43 @@ const SIGNING_DOMAIN_VERSION = "1"
  * LazyMinter is a helper class that creates NFTVoucher objects and signs them, to be redeemed later by the LazyNFT contract.
  */
 class LazyMinter {
-
   /**
    * Create a new LazyMinter targeting a deployed instance of the LazyNFT contract.
-   * 
+   *
    * @param {Object} options
    * @param {ethers.Contract} contract an ethers Contract that's wired up to the deployed contract
    * @param {ethers.Signer} signer a Signer whose account is authorized to mint NFTs on the deployed contract
    */
   constructor({ contractAddress, signer }) {
-    this.contractAddress = contractAddress
-    this.signer = signer
+    this.contractAddress = contractAddress;
+    this.signer = signer;
   }
 
   /**
    * Creates a new NFTVoucher object and signs it using this LazyMinter's signing key.
-   * 
+   *
    * @param {ethers.BigNumber | number} tokenId the id of the un-minted NFT
    * @param {string} uri the metadata URI to associate with this NFT
    * @param {ethers.BigNumber | number} minPrice the minimum price (in wei) that the creator will accept to redeem this NFT. defaults to zero
-   * 
+   *
    * @returns {NFTVoucher}
    */
-  async createVoucher(presale, cid, minPrice = 0) {
-    const voucher = { presale, cid, minPrice }
-    const domain = await this._signingDomain()
+  async createVoucher(tokenId, nonce, expiry) {
+    const voucher = { tokenId, nonce, expiry };
+    const domain = await this._signingDomain();
     const types = {
       NFTVoucher: [
-        {name: "presale", type: "bool"},
-        {name: "minPrice", type: "uint256"},
-        {name: "cid", type: "string"},  
-      ]
-    }
-    const signature = await this.signer._signTypedData(domain, types, voucher)
+        { name: "tokenId", type: "uint256" },
+        { name: "nonce", type: "uint256" },
+        { name: "expiry", type: "uint256" },
+      ],
+    };
+
+    const signature = await this.signer._signTypedData(domain, types, voucher);
     return {
       ...voucher,
       signature,
-    }
+    };
   }
 
   /**
@@ -63,19 +63,19 @@ class LazyMinter {
    */
   async _signingDomain() {
     if (this._domain != null) {
-      return this._domain
+      return this._domain;
     }
-    const chainId = 1;
+    const chainId = 4;
     this._domain = {
       name: SIGNING_DOMAIN_NAME,
       version: SIGNING_DOMAIN_VERSION,
       verifyingContract: this.contractAddress,
       chainId,
-    }
-    return this._domain
+    };
+    return this._domain;
   }
 }
 
 module.exports = {
-  LazyMinter
-}
+  LazyMinter,
+};
